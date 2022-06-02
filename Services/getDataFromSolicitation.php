bf22a0635f91382961af894e4237beca677e4053
Winicius Leal <winicius.leal@soluti.com.br>
2022-06-02 10:58:42 -0300

    adcionando novo servico getDataFromSolicitation da WebservcieArController

--- Services/getDataFromSolicitation.php
+++ Services/getDataFromSolicitation.php
@@ -4,16 +4,16 @@ require_once('vendor/autoload.php');
 use HTTP_Request2;
 
 $base_url = "https://crm-dev.lab.ca.inf.br/";
-$uri = "{$base_url}/webservice-api/consulta-dados-solicitacao";
+$uri = "{$base_url}/webservice-ar/get-data-from-solicitations";
 $method = 'POST';
 $hmacVersion = 1;
 $clientId = 0;
 
-$m = json_encode(array("solicitacao" => "18672202105802B0"));
+$m = json_encode(array("dataInicial" => "2022-05-01", "dataFinal" => "2022-06-01"));
 
 $ds = $method . $uri . $m;
 
-$key = "CHAVESECRETAAPI";
+$key = "PAULO123";
 $nonce = time();
 $hkey = $nonce . $key;
 $hmac = hash('sha256',hash('sha256', $hkey) . hash('sha256', $hkey . $ds));
