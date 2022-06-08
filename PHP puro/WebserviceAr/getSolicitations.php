882eb68e109757c61521ea3e9852dab62f824960
Winicius Leal <winicius.leal@soluti.com.br>
2022-06-08 16:57:35 -0300

    alterando diretorios

---
+++ PHP puro/WebserviceAr/getSolicitations.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('vendor/autoload.php');
+use HTTP_Request2;
+
+$base_url = "https://crm-dev.lab.ca.inf.br/";
+$uri = "{$base_url}/webservice-ar/get-solicitations";
+$method = 'POST';
+$hmacVersion = 1;
+$clientId = 0;
+
+$m = json_encode(array("dataInicial" => "2022-06-03", "dataFinal" => "2022-06-08"));
+
+$ds = $method . $uri . $m;
+
+$key = "CHAVEAPIAR";
+$nonce = time();
+$hkey = $nonce . $key;
+$hmac = hash('sha256',hash('sha256', $hkey) . hash('sha256', $hkey . $ds));
+
+$header = [
+    'HMAC-Authentication' => $hmacVersion . ':' . $clientId . ':' . $nonce . ':' . $hmac
+];
+
+$request = new HTTP_Request2();
+$request->setUrl($uri);
+$request->setMethod(HTTP_Request2::METHOD_POST);
+$request->setHeader($header);
+$request->setBody($m);
+
+$request->setConfig(array(
+    'ssl_verify_peer'   => false,
+    'ssl_verify_host'   => false
+));
+
+$response = $request->send();
+
+echo "\n";
+echo $response->getBody() . "\n\n";
